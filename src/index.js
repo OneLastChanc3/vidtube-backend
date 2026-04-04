b3f360177387572b72e2914db13fc0ed708ba004
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-04-04 00:17:45 -0500

    coneccion a DB

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("ghola")
+import { app } from "./app.js"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config ({ 
+    "path" : "./.env"
+})
+
+const PORT = process.env.PORT || 8001
+
+connectDB()
+    .then(() => { 
+        app.listen(PORT, () => {
+            console.log("en funcionamiento")
+        })
+    })
+    .catch((err) =>{ 
+    console.log("error in conection ", err)
+    })
