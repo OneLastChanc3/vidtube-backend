520c35eee0f5488ecfa5d23472944c1fe090a8e8
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-04-06 09:34:47 -0500

    schema

--- src/routes/healthcheack.routes.js
+++ src/routes/healthcheack.routes.js
@@ -4,5 +4,5 @@ import { healthcheck } from "../controllers/healthcheak.controllers.js";
 const router = Router()
 
 router.route("/").get(healthcheck)
-router.route("/test").get(healthcheck)
+router.route("/test").get(healthcheck )
 export default router
