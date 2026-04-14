f8a87b8ba7d7f8937edd3d4533fd1361611a896d
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-04-13 20:03:56 -0500

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import cloudinary from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME ,
+    api_key:process.env.CLOUDINARY_API_KEY ,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => { 
+    try {
+        if (!localFilePath) return null
+        const response = await cloudinary.uploader.upload(
+            localFilePath, {
+                resorce_type: "auto"
+            }
+        )
+        console.log("upload on cloudinary src: " + response.url)
+        fs.unlinkSync(localFilePath)
+        return response
+    } catch (err) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
