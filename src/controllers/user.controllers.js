2764171c0c4efeac72b50b3b9e241210806b0d1b
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-04-19 12:49:16 -0500

    comit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.models.js"
 import { deletedFromCoudinary, uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+const generateAccessToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new ApiError(400, "user not found")
+        }
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validareBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     const { fullname, username, email, password } = req.body
     if (
@@ -20,7 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "user or email already exist")
     }
 
-    console.warn(req,files)
+    console.warn(req, files)
     const avatarLocalPath = await req.files?.avatar?.[0]?.path
     const coverLocalPath = await req.files?.coverImage?.[0]?.path
 
@@ -37,7 +54,7 @@ const registerUser = asyncHandler(async (req, res) => {
         avatar = await uploadOnCloudinary(avatarLocalPath)
         console.log("Upload avatar", avatar)
     }
-    catch(error) { 
+    catch (error) {
         console.log("Error uploading avatar", error)
         throw new ApiError(500, "Failed to upload Avatar")
     }
@@ -51,39 +68,47 @@ const registerUser = asyncHandler(async (req, res) => {
         console.log("Error uploading coverImage", error)
         throw new ApiError(500, "Failed to upload coverImage")
     }
-try {
-    const user = await User.create({
-        fullname,
-        avatar: avatar.url,
-        converImage: coverImage?.url || "",
-        email,
-        password,
-        username: username.tolowerCase()
+    try {
+        const user = await User.create({
+            fullname,
+            avatar: avatar.url,
+            converImage: coverImage?.url || "",
+            email,
+            password,
+            username: username.tolowerCase()
 
-    })
+        })
 
-    const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-    )
-    if (!createdUser) {
-        throw new ApiError(500, "Somenthing went wrong while registering a user")
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        )
+        if (!createdUser) {
+            throw new ApiError(500, "Somenthing went wrong while registering a user")
+        }
+
+        return res
+            .status(201)
+            .json(new ApiResponse(200, createdUser, "User registed succesfully"))
+    } catch (error) {
+        console.log("User creation failed", error)
+
+        if (avatar) {
+            await deletedFromCoudinary(avatar.public_id)
+        }
+        if (coverImage) {
+            await deletedFromCoudinary(coverImage.public_id)
+        }
+        throw new ApiError(500, "Something went wrong while registering")
     }
 
-    return res
-        .status(201)
-        .json(new ApiResponse(200, createdUser, "User registed succesfully"))
-} catch (error) {
-    console.log("User creation failed", error)
-    
-    if (avatar) { 
-        await deletedFromCoudinary(avatar.public_id)
-    }
-    if (coverImage) { 
-        await deletedFromCoudinary(coverImage.public_id)
-    }
-    throw new ApiError(500, "Something went wrong while registering")
-}
-    
 })
 
-export { registerUser}
+const loginUser = asyncHandler(async (req, res) => { 
+    const { email, username, password } = req.body
+    
+    if (!email) { 
+        
+    }
+})
+
+export { registerUser }
